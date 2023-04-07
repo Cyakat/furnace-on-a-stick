fd92738b999354664da24d6ae3a38acc5dd1c093
Cyakat <noah@thebauers.net>
2023-04-07 10:22:30 -0600

    Initial Commit
    Mod is working as intended

---
+++ src/main/java/net/cyakat/furnaceonastick/FurnaceOnAStickClient.java
@@ -0,0 +1,10 @@
+package net.cyakat.furnaceonastick;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FurnaceOnAStickClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
